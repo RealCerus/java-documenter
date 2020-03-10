67bd56f60529fe5b7c6db0f8a9d348a291772529
Cerus <zocks1703@web.de>
2020-03-10 22:29:34 +0100

    Generated docs and updated script

--- src/main/java/example/generatedocs.bat
+++ src/main/java/example/generatedocs.bat
@@ -1,2 +1,2 @@
 @echo off
-java -jar ..\target\javadocsgenerator-1.0.0.jar --files=Person.java,Pet.java --output=README.md
+java -jar ..\..\..\..\target\javadocsgenerator-1.0.1.jar --files=Person.java,Pet.java --output=README.md
