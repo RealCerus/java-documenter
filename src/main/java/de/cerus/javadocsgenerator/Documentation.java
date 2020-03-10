4d319d8554aa439cfb8ac7c0e926cb563763955f
Cerus <zocks1703@web.de>
2020-03-10 21:22:58 +0100

    Initial commit

---
+++ src/main/java/de/cerus/javadocsgenerator/Documentation.java
@@ -0,0 +1,179 @@
+package de.cerus.javadocsgenerator;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Documentation {
+
+    private AccessModifier accessModifier;
+    private boolean finalModifier, staticModifier, abstractModifier, syncModifier, nativeModifier, strictfpModifier;
+    private String returnType, name;
+    private Parameter[] parameters;
+    private String description;
+
+    public Documentation(Method method) {
+        int modifiers = method.getModifiers();
+
+        // Parses the access modifier
+        AccessModifier accessModifier;
+        if (Modifier.isPublic(modifiers)) {
+            accessModifier = AccessModifier.PUBLIC;
+        } else if (Modifier.isPrivate(modifiers)) {
+            accessModifier = AccessModifier.PRIVATE;
+        } else if (Modifier.isProtected(modifiers)) {
+            accessModifier = AccessModifier.PROTECTED;
+        } else {
+            accessModifier = AccessModifier.PACKAGE_PRIVATE;
+        }
+        this.accessModifier = accessModifier;
+
+        // Parses every other modifier
+        this.finalModifier = Modifier.isFinal(modifiers);
+        this.abstractModifier = Modifier.isAbstract(modifiers);
+        this.staticModifier = Modifier.isStatic(modifiers);
+        this.syncModifier = Modifier.isSynchronized(modifiers);
+        this.strictfpModifier = Modifier.isStrict(modifiers);
+
+        // Parses the return type
+        Class<?> returnTypeClass = method.getReturnType();
+        this.returnType = returnTypeClass.getSimpleName();
+
+        // Parsed the name
+        this.name = method.getName();
+
+        // Parses the method parameters
+        this.parameters = new Parameter[method.getParameterCount()];
+        for (int i = 0; i < method.getParameters().length; i++) {
+            java.lang.reflect.Parameter param = method.getParameters()[i];
+            parameters[i] = new Parameter(param.getType().getSimpleName(), param.getType().isArray(), param.isVarArgs());
+        }
+
+        this.description = "";
+    }
+
+    public Documentation(AccessModifier accessModifier, boolean finalModifier, boolean staticModifier,
+                         boolean abstractModifier, boolean syncModifier, boolean nativeModifier,
+                         boolean strictfpModifier, String returnType, String name, Parameter[] parameters, String description) {
+        this.accessModifier = accessModifier;
+        this.finalModifier = finalModifier;
+        this.staticModifier = staticModifier;
+        this.abstractModifier = abstractModifier;
+        this.syncModifier = syncModifier;
+        this.nativeModifier = nativeModifier;
+        this.strictfpModifier = strictfpModifier;
+        this.returnType = returnType;
+        this.name = name;
+        this.parameters = parameters;
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return accessModifier.toString()
+                + (hasStaticModifier() ? "static " : "")
+                + (hasAbstractModifier() ? "abstract " : "")
+                + (hasFinalModifier() ? "final " : "")
+                + (hasNativeModifier() ? "native " : "")
+                + (hasStrictfpModifier() ? "strictfp " : "")
+                + (hasSyncModifier() ? "synchronized " : "")
+                + returnType + " " + name + "("
+                + Arrays.stream(parameters)
+                .map(Parameter::toString)
+                .collect(Collectors.joining(", "))
+                + ")";
+    }
+
+    public String toMarkdownString() {
+        return "### " + toString().replace("<", "\\<") + '\n' +
+                (description == null || description.equals("")
+                        ? "*No description available*" : "```\n" + description + "\n```");
+    }
+
+    public AccessModifier getAccessModifier() {
+        return accessModifier;
+    }
+
+    public boolean hasFinalModifier() {
+        return finalModifier;
+    }
+
+    public boolean hasStaticModifier() {
+        return staticModifier;
+    }
+
+    public boolean hasAbstractModifier() {
+        return abstractModifier;
+    }
+
+    public boolean hasSyncModifier() {
+        return syncModifier;
+    }
+
+    public boolean hasNativeModifier() {
+        return nativeModifier;
+    }
+
+    public boolean hasStrictfpModifier() {
+        return strictfpModifier;
+    }
+
+    public String getReturnType() {
+        return returnType;
+    }
+
+    public Parameter[] getParameters() {
+        return parameters;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public static class Parameter {
+        public String className;
+        public boolean array, varargs;
+
+        public Parameter(String className, boolean array, boolean varargs) {
+            this.className = className;
+            this.array = array;
+            this.varargs = varargs;
+        }
+
+        public Parameter() {
+        }
+
+        @Override
+        public String toString() {
+            return className.substring(0, varargs ? className.length() - 2 : className.length())
+                    + (varargs ? "..." : "");
+        }
+    }
+
+    enum AccessModifier {
+        PACKAGE_PRIVATE,
+        PRIVATE,
+        PROTECTED,
+        PUBLIC;
+
+        @Override
+        public String toString() {
+            switch (this) {
+                default:
+                case PACKAGE_PRIVATE:
+                    return "";
+                case PRIVATE:
+                    return "private ";
+                case PROTECTED:
+                    return "protected ";
+                case PUBLIC:
+                    return "public ";
+            }
+        }
+    }
+}
---
+++ src/main/java/de/cerus/javadocsgenerator/FileScanner.java
@@ -0,0 +1,143 @@
+package de.cerus.javadocsgenerator;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FileScanner {
+
+    private List<String> contents;
+
+    public FileScanner(File file) throws IOException {
+        contents = Files.readAllLines(file.toPath());
+    }
+
+    // Scans the file contents for any documentation
+    public List<Documentation> scan() {
+        // Looping through every line
+        for (int lineNo = 0; lineNo < contents.size(); lineNo++) {
+            String line = contents.get(lineNo);
+
+            // Checks if line is a valid class declaration, returns null if no '//DOC' comment is specified
+            if (line.matches("(private |public |protected |)(static)?class [A-Za-z0-9_]+ \\{")) {
+                String lineBefore = contents.get(lineNo - 1);
+                if (!lineBefore.equalsIgnoreCase("//DOC")) return null;
+            }
+        }
+
+        List<Documentation> documentations = new ArrayList<>();
+
+        // Loops through every line
+        for (int lineNo = 0; lineNo < contents.size(); lineNo++) {
+            String line = contents.get(lineNo);
+
+            // Checks if line is a valid method head
+            // If true: Scans the method head
+            if (line.trim().matches("(private |public |protected |)((native )?(abstract )?(strictfp )" +
+                    "?(synchronized )?(static )?(final )?)[A-Za-z0-9_ <>?]+\\(([A-Za-z0-9_\\[\\]., <>?]+)?\\) \\{")) {
+                scanMethod(lineNo, documentations);
+            }
+        }
+
+        return documentations;
+    }
+
+    // Scans a method head and parses it
+    private void scanMethod(int lineNo, List<Documentation> documentations) {
+        boolean flag = false;
+
+        // Set flag if method has '//DOC' comment
+        if (contents.get(lineNo - 1).trim().equalsIgnoreCase("//DOC")) {
+            flag = true;
+        }
+
+        List<String> descList = new ArrayList<>();
+
+        // If no '//DOC' comment is found check for a valid java doc comment
+        if (!flag) {
+            int ogLineNo = lineNo;
+            String prevLine = contents.get(lineNo - 1);
+
+            // Checks if the line is a valid ending of a java doc comment
+            if (prevLine.trim().equals("*/")) {
+                lineNo -= 2;
+                String line;
+
+                // Loops through every line of the java doc comment
+                // Adds the line to the description list
+                while ((line = contents.get(lineNo)).trim().startsWith("*")) {
+                    descList.add(line.substring(line.indexOf("*") + 1).trim());
+                    lineNo--;
+                }
+
+                // Checks if the line is a valid beginning of a java doc comment
+                // If true: Sets flag to true
+                if (contents.get(lineNo).trim().equals("/**")) {
+                    flag = true;
+                    lineNo = ogLineNo;
+                    Collections.reverse(descList);
+                }
+            }
+        }
+
+        // Returns if neither a '//DOC' nor a valid java doc comment is found
+        if (!flag) return;
+
+        String line = contents.get(lineNo).trim();
+
+        // Parses the access modifier
+        Documentation.AccessModifier accessModifier;
+        if (line.contains("public")) {
+            accessModifier = Documentation.AccessModifier.PUBLIC;
+        } else if (line.contains("private")) {
+            accessModifier = Documentation.AccessModifier.PRIVATE;
+        } else if (line.contains("protected")) {
+            accessModifier = Documentation.AccessModifier.PROTECTED;
+        } else {
+            accessModifier = Documentation.AccessModifier.PACKAGE_PRIVATE;
+        }
+        line = line.replace(accessModifier.toString(), "");
+
+        // Parses every other modifier
+        boolean abstractModifier = line.contains("abstract");
+        if (abstractModifier) line = line.replace("abstract ", "");
+        boolean finalModifier = line.contains("final");
+        if (finalModifier) line = line.replace("final ", "");
+        boolean staticModifier = line.contains("static");
+        if (staticModifier) line = line.replace("static ", "");
+        boolean syncModifier = line.contains("sync");
+        if (syncModifier) line = line.replace("sync ", "");
+        boolean strictfpModifier = line.contains("strictfp");
+        if (strictfpModifier) line = line.replace("strictfp ", "");
+        boolean nativeModifier = line.contains("native");
+        if (nativeModifier) line = line.replace("native ", "");
+
+        // Parsed the return type
+        String returnType = line.trim().substring(0, line.indexOf(" "));
+        line = line.substring(returnType.length() + 1);
+
+        // Parses the method name
+        String name = line.substring(0, line.indexOf("("));
+        line = line.substring(name.length() + 1, line.indexOf(")"));
+
+        line = line.replaceAll(",\\s+", ",");
+
+        // Parses the method parameters
+        String[] rawParams = line.split(",");
+        Documentation.Parameter[] parameters = new Documentation.Parameter[rawParams.length];
+        for (int i = 0; i < rawParams.length; i++) {
+            String rawParam = rawParams[i].split(" ")[0];
+            boolean array = rawParam.endsWith("[]") || rawParam.endsWith("...");
+            boolean varargs = rawParam.endsWith("...");
+            parameters[i] = new Documentation.Parameter(rawParam, array, varargs);
+        }
+
+        // Adds the parsed documentation to the documentations list
+        documentations.add(new Documentation(accessModifier, finalModifier, staticModifier, abstractModifier,
+                syncModifier, nativeModifier, strictfpModifier, returnType, name, parameters,
+                descList.isEmpty() ? "" : String.join("\n", descList)));
+    }
+}
